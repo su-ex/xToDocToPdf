d461396fd045d70e309df6f5a88cf69e316f35d8
su-ex <codeworks@supercable.onl>
2020-06-17 11:05:31 +0200

    Enqueue jobs to show progress

--- src/xToDoc.ps1
+++ src/xToDoc.ps1
@@ -12,15 +12,22 @@ $description | Format-Table
 $path = "X:\Projekte\2020\PR-2000158_IMB Stromversorgungssysteme GmbH_Test Bedienhandbuch\TestBedienhandbuch"
 $target = "$path\Ziel.docx"
 
-$WA = initWA
+$functions = [System.Collections.Queue]@()
 
-Copy-Item "$path\FormatvorlagenUndAnfang.docx" -Destination "$target" -Force
-$WA.Run("concatenate", [ref]$target, [ref]"$path\WichtigeInformation.doc")
-$WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc")
-$WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc")
-$WA.Run("concatenate", [ref]$target, [ref]"$path\Rest.doc")
-$WA.Run("updateHeadings", [ref]$target)
-$WA.Run("updateFields", [ref]$target)
-$WA.Run("saveAndClose", [ref]$target)
+$functions.Enqueue({ $Script:WA = initWA })
+$functions.Enqueue({ Copy-Item "$path\FormatvorlagenUndAnfang.docx" -Destination "$target" -Force })
+$functions.Enqueue({ $WA.Run("concatenate", [ref]$target, [ref]"$path\WichtigeInformation.doc") })
+$functions.Enqueue({ $WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc") })
+$functions.Enqueue({ $WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc") })
+$functions.Enqueue({ $WA.Run("concatenate", [ref]$target, [ref]"$path\Rest.doc") })
+$functions.Enqueue({ $WA.Run("updateHeadings", [ref]$target) })
+$functions.Enqueue({ $WA.Run("updateFields", [ref]$target) })
+$functions.Enqueue({ $WA.Run("saveAndClose", [ref]$target) })
+$functions.Enqueue({ destroyWA })
 
-destroyWA
+$total = $functions.Count
+$i = 0
+while ($functions.Count -gt 0) {
+    Write-Progress -Activity "Generiere Word-Dokument ..." -Status "$($i+1) / $total" -PercentComplete (($i++/$total)*100) -CurrentOperation "Tue irgendwas"
+    & $functions.Dequeue()
+}
