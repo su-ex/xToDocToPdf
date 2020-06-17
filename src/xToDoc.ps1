e129b44c6fa4b1d6fb26efd80ec38befc0cc1b9e
su-ex <codeworks@supercable.onl>
2020-06-17 15:04:40 +0200

    Always try to destroy Word abstraction

--- src/xToDoc.ps1
+++ src/xToDoc.ps1
@@ -13,17 +13,19 @@ $description | Format-Table
 $path = "X:\Projekte\2020\PR-2000158_IMB Stromversorgungssysteme GmbH_Test Bedienhandbuch\TestBedienhandbuch"
 $target = "$path\Ziel.docx"
 
+$WA = initWA
+
 $jobs = JobHandling("Generiere Word-Dokument ...")
 
-$jobs.add("Tue irgendwas", { $Script:WA = initWA })
 $jobs.add("Tue irgendwas", { Copy-Item "$path\FormatvorlagenUndAnfang.docx" -Destination "$target" -Force })
 $jobs.add("Tue irgendwas", { $WA.Run("concatenate", [ref]$target, [ref]"$path\WichtigeInformation.doc") })
 $jobs.add("Tue irgendwas", { $WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc") })
 $jobs.add("Tue irgendwas", { $WA.Run("concatenate", [ref]$target, [ref]"$path\blablabla.doc") })
-$jobs.add("Tue irgendwas", { $WA.Run("concatenate", [ref]$target, [ref]"$path\Rest.doc") })
+$jobs.add("Tue irgendwas", { $WA.Run("concatenate", [ref]$target, [ref]"$path\Rest.doci") })
 $jobs.add("Tue irgendwas", { $WA.Run("updateHeadings", [ref]$target) })
 $jobs.add("Tue irgendwas", { $WA.Run("updateFields", [ref]$target) })
 $jobs.add("Tue irgendwas", { $WA.Run("saveAndClose", [ref]$target) })
-$jobs.add("Tue irgendwas", { destroyWA })
 
 $jobs.run()
+
+destroyWA
