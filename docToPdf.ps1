0fb8a535d1f19278263d2a172b22ad31f09387e0
su-ex <codeworks@supercable.onl>
2020-06-26 15:43:33 +0200

    Initial Word to PDF stuff

---
+++ docToPdf.ps1
@@ -0,0 +1,64 @@
+﻿Param (
+    [String] ${working-directory} = "X:\Projekte\2020\PR-2000158_IMB Stromversorgungssysteme GmbH_Test Bedienhandbuch\TestBedienhandbuch",
+    
+    [String] ${source-word-file} = ".\Ziel.docx",
+    [String] ${target-pdf-file} = ""
+)
+
+if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
+    $Script:ScriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+} else {
+    $Script:ScriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0]) 
+    if (!$ScriptPath) { $Script:ScriptPath = "." }
+}
+
+Import-Module "$ScriptPath\modules\WordAbstraction.psm1" -Force
+Import-Module "$ScriptPath\modules\ProgressHelper.psm1" -Force
+Import-Module "$ScriptPath\modules\PdfHelper.psm1" -Force
+Import-Module "$ScriptPath\modules\WordPdfExportHelper.psm1" -Force
+Import-Module "$ScriptPath\modules\HelperFunctions.psm1" -Force
+
+# make sure working directory exists and make path always absolute
+try {
+    $Script:workingDirectory = Resolve-Path ${working-directory} -ErrorAction Stop
+} catch {
+    Write-Error "Das Arbeitsverzeichnis existiert nicht: $_"
+    exit -1
+}
+
+# base source file upon working directory
+$sourceWordFile = makePathAbsolute $workingDirectory ${source-word-file}
+if (-not (Test-Path $sourceWordFile)) {
+    Write-Error "Das Quelldokument $sourceWordFile existiert nicht!"
+    exit -1
+}
+
+# base target file upon working directory from 
+$targetPdfFile = makePathAbsolute $workingDirectory ([System.IO.Path]::ChangeExtension(${source-word-file}, "pdf"))
+if (${target-pdf-file} -ne "") {
+    $targetPdfFile = makePathAbsolute $workingDirectory ${target-pdf-file}
+
+    if (-not (Split-Path $targetPdfFile | Test-Path)) {
+        Write-Error "Das Verzeichnis, in dem die PDF-Datei gespeichert werden soll, existiert nicht!"
+        exit -1
+    }
+}
+
+# $WA = WordAbstraction
+
+# $progress = ProgressHelper("Generiere Word-Dokument ...")
+# $progress.setTotalOperations($totalOperations)
+
+# try {
+    getPdfReplacementPages $sourceWordFile
+# } catch {
+#     try { $WA.saveAndClose($targetFile) | Out-Null } catch {}
+#     Write-Error "Zusammensetzen leider fehlgeschlagen: $($_.Exception.Message)"
+#     exit -1
+# } finally {
+#     $WA.destroy()
+# }
+
+# $progress.success()
+
+exit 0
--- modules/PdfHelper.psm1
+++ modules/PdfHelper.psm1
@@ -4,3 +4,7 @@ Function getPdfPageNumber($file) {
     #Write-Host "exe: $qpdfexe, file: $file"
     return [int](& "$qpdfexe" --show-npages "`"$file`"")
 }
+
+Function overlayPages($replacements){
+    return [int](& "$qpdfexe" 'X:\Projekte\2020\PR-2000158_IMB Stromversorgungssysteme GmbH_Test Bedienhandbuch\TestBedienhandbuch\Ziel.pdf' --overlay 'X:\Vorlagen\Bedienhandbuch\de\SaudiArab60-100.pdf' --from=1-14 --to=4-17 -- 'X:\Projekte\2020\PR-2000158_IMB Stromversorgungssysteme GmbH_Test Bedienhandbuch\TestBedienhandbuch\Zielersatz.pdf')
+}
