008f3a48b86a6ea76ce43786647dcbf11e1dbd15
su-ex <codeworks@supercable.onl>
2020-06-30 08:57:17 +0200

    New PS7 syntax -> cleanup

--- modules/HelperFunctions.psm1
+++ modules/HelperFunctions.psm1
@@ -1,8 +1,3 @@
-# see: https://stackoverflow.com/questions/25682507/powershell-inline-if-iif
-Function IIf($If, $Right, $Wrong) {If ($If) {$Right} Else {$Wrong}}
-
-Function ItIf($If, $Wrong) {If ($If) {$If} Else {$Wrong}}
-
 # see: https://powershellexplained.com/2017-01-13-powershell-variable-substitution-in-strings/
 Function replaceTokens([String]$placeholderString, [hashtable]$tokens) {
     foreach ($token in $tokens.GetEnumerator()) {
